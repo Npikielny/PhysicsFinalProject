b202afc3662b79ead4c69fa372986a81fae0c96f
Noah Pikielny <npikielny@gmail.com>
2019-05-02 13:49:39 -0400

    Initial Commit

---
+++ PhysicsProgrammatic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhysicsProgrammatic
+//
+//  Created by Noah Pikielny on 5/2/19.
+//  Copyright © 2019 Noah Pikielny. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PhysicsProgrammatic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PhysicsProgrammatic
+//
+//  Created by Noah Pikielny on 5/2/19.
+//  Copyright © 2019 Noah Pikielny. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
